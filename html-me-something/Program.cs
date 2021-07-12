61921237c0c4c5d42e0763904040eb2ab21316a3
GingerRanger <86305723+GingerRanger@users.noreply.github.com>
2021-07-12 20:30:21 +0200

    Add files via upload

---
+++ html-me-something/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace html_me_something
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
